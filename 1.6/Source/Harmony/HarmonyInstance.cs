4f791981202c3dc7a1b29b475353baaf6f3d43de
juanosarg <31105173+juanosarg@users.noreply.github.com>
2026-06-08 19:00:20 +0200

    Initial commit

---
+++ 1.6/Source/Harmony/HarmonyInstance.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using HarmonyLib;
+using System.Reflection;
+namespace VanillaPlantsExpandedFlowers
+{
+    [StaticConstructorOnStartup]
+    public class Main
+    {
+        static Main()
+        {
+            var harmony = new Harmony("com.VanillaPlantsExpandedFlowers");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
