deacae0fec8b5d3c12c3079f76ac489fa834e74c
juanosarg <31105173+juanosarg@users.noreply.github.com>
2026-06-13 13:04:23 +0200

    v0.1

---
+++ 1.6/Source/AI/JobDrivers/JobDriver_SmellingFlowers.cs
@@ -0,0 +1,19 @@
+﻿using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace VanillaPlantsExpandedFlowers
+{
+    public class JobDriver_SmellingFlowers : JobDriver_VisitJoyThing
+    {
+        private Thing Flower => job.GetTarget(TargetIndex.A).Thing;
+
+        protected override void WaitTickAction(int delta)
+        {
+            float num = Flower.GetStatValue(StatDefOf.Beauty) / Flower.def.GetStatValueAbstract(StatDefOf.Beauty);
+            float extraJoyGainFactor = ((num > 0f) ? num : 0f);
+            pawn.GainComfortFromCellIfPossible(delta);
+            JoyUtility.JoyTickCheckEnd(pawn, delta, JoyTickFullJoyAction.EndJob, extraJoyGainFactor);
+        }
+    }
+}
---
+++ 1.6/Source/AI/JoyGivers/JoyGiver_SmellingFlowers.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using RimWorld;
+using UnityEngine;
+using VEF.Plants;
+using Verse;
+using Verse.AI;
+
+namespace VanillaPlantsExpandedFlowers
+{
+    public class JoyGiver_SmellingFlowers : JoyGiver
+    {
+        private static readonly List<Thing> candidates = new List<Thing>();
+
+        public override Job TryGiveJob(Pawn pawn)
+        {
+          
+            bool allowedOutside = JoyUtility.EnjoyableOutsideNow(pawn);
+            try
+            {
+                candidates.AddRange(pawn.Map.listerThings.ThingsInGroup(ThingRequestGroup.Plant).Where(delegate (Thing thing)
+                {
+                    if (!Validator(thing))
+                    {
+                        
+                        return false;
+                    }               
+                  
+                    return true;
+                }));
+                if (!candidates.TryRandomElementByWeight((Thing target) => Math.Max(target.GetStatValue(StatDefOf.Beauty), 0.5f), out var result))
+                {
+                    return null;
+                }
+                return JobMaker.MakeJob(def.jobDef, result);
+            }
+            finally
+            {
+                candidates.Clear();
+            }
+            bool Validator(Thing thing)
+            {
+               
+                Plant_Blooming plant = thing as Plant_Blooming;
+                if (plant is null) return false; 
+                if (!plant.isBlooming) return false;
+                
+                if (!thing.Fogged()  && pawn.CanReserveAndReach(thing, PathEndMode.Touch, Danger.None))
+                {
+                  
+                    return !thing.IsForbidden(pawn);
+                }
+                return false;
+            }
+        }
+    }
+}
